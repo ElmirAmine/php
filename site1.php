abad0a2fd65aa5be305a2f06b875928c2d961502
EL_MIR AMINE <el_mir_a@JEAN-ROSTAND.lan>
2024-10-09 15:32:51 +0200

    first commit

---
+++ site1.php
@@ -0,0 +1,29 @@
+<?php
+
+  function helloWorld(){
+    echo "Hello World!"
+    ; 
+  }
+
+  function quiEstLeMeilleurProf(){
+    echo "Le prof de programmation Web"
+    ; 
+  }
+
+  function jeRetourneMonArgument($arg1){
+    return $arg1
+        ; 
+    }
+    return jeRetourneMonArgument("abc");
+    return jeRetourneMonArgument(123);
+
+    function concatenation($arg1, $arg2){
+        echo $arg1 . $arg2
+        ; 
+    }
+    return concatenation("Antoine" , "Griezman");    
+
+  echo concatenation ($arg1, $arg2)
+?>
+
+
