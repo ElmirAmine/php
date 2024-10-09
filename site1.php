c29068ad9cea192f91c34a60eb8a786072492636
EL_MIR AMINE <el_mir_a@JEAN-ROSTAND.lan>
2024-10-09 15:49:24 +0200

    exercice5

--- site1.php
+++ site1.php
@@ -25,5 +25,11 @@
 
   echo concatenation ($arg1, $arg2)
 ?>
+<?php
+function concatenationAvecEspace($arg1, $arg2){
+    return $arg1 . "  " . $arg2;
 
+}
+echo concatenationAvecEspace("Ngolo ", "Kante");
+?>
 
