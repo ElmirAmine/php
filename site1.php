263e5ecfeda05faa86568cfd072ae06129bd8618
EL_MIR AMINE <el_mir_a@JEAN-ROSTAND.lan>
2024-10-09 16:57:39 +0200

    Description des changements

--- site1.php
+++ site1.php
@@ -32,4 +32,69 @@ function concatenationAvecEspace($arg1, $arg2){
 }
 echo concatenationAvecEspace("Ngolo ", "Kante");
 ?>
+<?php
+function somme($num1, $num2){
+    $resultat=$num1 + $num2;
+    return $resultat ;
+}
+echo somme(5,5);
 
+<?php
+function soustraction($num1, $num2){
+    $resultat=$num1 - $num2;
+    return $resultat ;
+}
+echo soustraction(5,5);
+
+?>
+<?php
+function multiplication($num1, $num2){
+    $resultat=$num1 * $num2;
+    return $resultat ;
+}
+echo multiplication(5,5);
+
+?>
+
+<?php
+function estIlMajeure($age){
+    if ($age >= 18){
+    return true;
+  } else {
+    return false;
+  }
+}
+echo estIlMajeure(5);
+echo estIlMajeure(34);
+
+?>
+<?php
+function plusGrand($num1, $num2) {
+    return $num1 > $num2 ? $num1 : $num2;
+}
+echo plusGrand(10, 20);
+?>
+<?php
+function plusPetit($num1, $num2) {
+    return $num1 < $num2 ? $num1 : $num2;
+}
+echo plusPetit(10, 20);
+?>
+
+<?php
+function plusPetit($num1, $num2, $num3) {
+    return ($num1 < $num2) ? (($num1 < $num3) ? $num1 : $num3) : (($num2 < $num3) ? $num2 : $num3);
+}
+echo plusPetit(10, 20, 30);
+
+?>
+
+<?php
+
+function premierElementTableau($tableau) {
+    return $tableau ? $tableau[0] : null;
+}
+
+echo premierElementTableau([10, 20, 30]);
+echo premierElementTableau([]) === null ? 'null' : premierElementTableau([]); 
+?>
